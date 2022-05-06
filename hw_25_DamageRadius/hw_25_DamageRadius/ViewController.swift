3d75e6a1285fa0370742d3912e4623b6b6667b1f
user-puser-1 <andrew101520@gmail.com>
2022-05-06 23:23:46 +0300

    Create test circle

--- hw_25_DamageRadius/hw_25_DamageRadius/ViewController.swift
+++ hw_25_DamageRadius/hw_25_DamageRadius/ViewController.swift
@@ -6,14 +6,76 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var buttonAppearance: UIButton!
+    
+    var circle = GMSCircle()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupButton(buttonAppearance)
+        mapView.layer.cornerRadius = 5
+        
+        mapView.isMyLocationEnabled = true
+        mapView.delegate = self
+        mapView.isIndoorEnabled = true
+    }
+    
+    private func setupButton(_ button: UIButton) {
+        
+        button.layer.cornerRadius = 5
+        button.layer.shadowColor = UIColor.gray.cgColor
+        button.layer.shadowOpacity = 1
+        button.layer.shadowOffset = .zero
+        button.layer.shadowRadius = 10
+        button.layer.rasterizationScale = UIScreen.main.scale
+    }
+    
+    private func createMarker(coordinates: CLLocationCoordinate2D) {
+        
+        let marker = GMSMarker(position: coordinates)
+        marker.title = "Координаты: \(coordinates.latitude), \(coordinates.longitude)"
+        mapView.clear()
+        marker.map = mapView
+        updateCamera(coordinate: coordinates)
+    }
+    
+    private func updateCamera(coordinate: CLLocationCoordinate2D) {
+        
+        let camera = GMSCameraPosition(latitude: coordinate.latitude, longitude: coordinate.longitude, zoom: mapView.camera.zoom)
+        mapView.animate(to: camera)
+    }
+    
+    private func createRadius(coordinate: CLLocationCoordinate2D, radius: CLLocationDistance) {
+        
+        let circleCenter = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        circle.position = circleCenter
+//        circle.fillColor = .systemRed
+        circle.strokeColor = .systemRed
+        circle.strokeWidth = 3
+        circle.radius = radius
+        circle.map = mapView
     }
 
-
+    @IBAction func setParametersAction(_ sender: UIButton) {
+    }
+        
 }
 
+extension ViewController: GMSMapViewDelegate {
+    
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        createMarker(coordinates: coordinate)
+        createRadius(coordinate: coordinate, radius: 1000)
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        
+        return true
+    }
+}
