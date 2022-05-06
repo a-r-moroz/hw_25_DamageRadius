e3b80c869f1d59821475dfbaaa8f15f0ddcb5a42
user-puser-1 <andrew101520@gmail.com>
2022-05-06 21:26:30 +0300

    Create xCode Project

---
+++ hw_25_DamageRadius/hw_25_DamageRadius/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw_25_DamageRadius
+//
+//  Created by Andrew Moroz on 6.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
